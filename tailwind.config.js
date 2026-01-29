6953d87e85492290b325494e3fe04508fa371fac
madeitaly <matteo@madmap.uk>
2026-01-30 00:21:15 +0100

    add tailwindcss support for layout

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This tells Tailwind where your HTML/React code is
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
