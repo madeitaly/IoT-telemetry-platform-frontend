1b8a1805a2be176dd558a9b56458e45443249c9b
madeitaly <matteo@madmap.uk>
2026-01-30 23:09:01 +0100

    change favicon, clean up from vite template

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
@@ -24,7 +21,6 @@ const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
 
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <AuthProvider>
